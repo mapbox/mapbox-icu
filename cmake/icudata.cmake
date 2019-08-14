dc7f95b1166141a9dd38c5c2da26b39bb9c29b41
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-08-14 14:23:26 +0300

    [icudata] Fix compilation order & recompilation

--- cmake/icudata.cmake
+++ cmake/icudata.cmake
@@ -1,48 +1,63 @@
-find_package(ICU 60.2)
+find_package(ICU QUIET)
 if (ICU_PKGDATA_EXECUTABLE)
-    add_custom_target(icudata-misc ALL
-        COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/icudt63l
-        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/icudt63l
-        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/unames.icu ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/unames.icu
-        COMMAND ${ICU_GENCNVAL_EXECUTABLE} -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l mappings/convrtrs.txt
-        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/coll/ucadata-unihan.icu ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/coll/ucadata.icu
-        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/nfkc.nrm ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/nfkc.nrm
-        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/nfkc_cf.nrm ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/nfkc_cf.nrm
-        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/uts46.nrm ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/uts46.nrm
-        COMMAND ${ICU_MAKECONV_EXECUTABLE} -c -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l mappings/ibm-37_P100-1995.ucm
-        COMMAND ${ICU_MAKECONV_EXECUTABLE} -c -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l mappings/ibm-1047_P100-1995.ucm
-        COMMAND ${ICU_GENRB_EXECUTABLE} -k -q -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s misc -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l numberingSystems.txt
-        COMMAND ${ICU_GENRB_EXECUTABLE} -k -q -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s misc -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l icuver.txt
-        COMMAND ${ICU_GENRB_EXECUTABLE} -k -q -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s misc -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l icustd.txt
+    set(_ICUDATA_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/icudt63l)
+
+    add_custom_command(
+        OUTPUT ${_ICUDATA_BINARY_DIR}
+        COMMAND rm -rf ${_ICUDATA_BINARY_DIR}
+        COMMAND mkdir -p ${_ICUDATA_BINARY_DIR}/curr ${_ICUDATA_BINARY_DIR}/lang ${_ICUDATA_BINARY_DIR}/region
+        COMMAND mkdir -p ${_ICUDATA_BINARY_DIR}/zone ${_ICUDATA_BINARY_DIR}/unit ${_ICUDATA_BINARY_DIR}/brkitr
+        COMMAND mkdir -p ${_ICUDATA_BINARY_DIR}/coll ${_ICUDATA_BINARY_DIR}/rbnf
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/unames.icu ${_ICUDATA_BINARY_DIR}/unames.icu
+        COMMAND ${ICU_GENCNVAL_EXECUTABLE} -d ${_ICUDATA_BINARY_DIR} mappings/convrtrs.txt
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/coll/ucadata-unihan.icu ${_ICUDATA_BINARY_DIR}/coll/ucadata.icu
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/nfkc.nrm ${_ICUDATA_BINARY_DIR}/nfkc.nrm
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/nfkc_cf.nrm ${_ICUDATA_BINARY_DIR}/nfkc_cf.nrm
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl in/uts46.nrm ${_ICUDATA_BINARY_DIR}/uts46.nrm
+        COMMAND ${ICU_MAKECONV_EXECUTABLE} -c -d ${_ICUDATA_BINARY_DIR} mappings/ibm-37_P100-1995.ucm
+        COMMAND ${ICU_MAKECONV_EXECUTABLE} -c -d ${_ICUDATA_BINARY_DIR} mappings/ibm-1047_P100-1995.ucm
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -q -i ${_ICUDATA_BINARY_DIR} -s misc -d ${_ICUDATA_BINARY_DIR} numberingSystems.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -q -i ${_ICUDATA_BINARY_DIR} -s misc -d ${_ICUDATA_BINARY_DIR} icuver.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -q -i ${_ICUDATA_BINARY_DIR} -s misc -d ${_ICUDATA_BINARY_DIR} icustd.txt
+        COMMAND ${ICU_GENCFU_EXECUTABLE} -c -i ${_ICUDATA_BINARY_DIR} -r unidata/confusables.txt -w unidata/confusablesWholeScript.txt -o ${_ICUDATA_BINARY_DIR}/confusables.cfu
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
     )
 
-    function(add_icudata_resource_target target)
-        add_custom_target(icudata-${target} ALL
-            COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/${target}
-            COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s generated/${target} -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/${target} res_index.txt
-            COMMAND test -f ${target}/pool.res && ${ICU_ICUPKG_EXECUTABLE} -tl ${target}/pool.res ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/${target}/pool.res || true
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
-        )
-    endfunction()
+    add_custom_command(
+        OUTPUT ${_ICUDATA_BINARY_DIR}/pool.res
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl locales/pool.res ${_ICUDATA_BINARY_DIR}/pool.res
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl curr/pool.res ${_ICUDATA_BINARY_DIR}/curr/pool.res
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl lang/pool.res ${_ICUDATA_BINARY_DIR}/lang/pool.res
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl region/pool.res ${_ICUDATA_BINARY_DIR}/region/pool.res
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl unit/pool.res ${_ICUDATA_BINARY_DIR}/unit/pool.res
+        COMMAND ${ICU_ICUPKG_EXECUTABLE} -tl zone/pool.res ${_ICUDATA_BINARY_DIR}/zone/pool.res
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
+        DEPENDS ${_ICUDATA_BINARY_DIR}
+    )
 
-    function(add_icudata_resource_files target)
-        foreach(file IN LISTS ${target}_files)
-            add_custom_command(TARGET icudata-${target} PRE_BUILD
-                COMMAND ${ICU_GENRB_EXECUTABLE} --usePoolBundle -k -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s ${target} -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/${target} ${file}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
-            )
-        endforeach()
-    endfunction()
+    add_custom_command(
+        OUTPUT ${_ICUDATA_BINARY_DIR}/res_index.res
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated -d ${_ICUDATA_BINARY_DIR} res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/lang -d ${_ICUDATA_BINARY_DIR}/lang res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/region -d ${_ICUDATA_BINARY_DIR}/region res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/zone -d ${_ICUDATA_BINARY_DIR}/zone res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/coll -d ${_ICUDATA_BINARY_DIR}/coll res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/brkitr -d ${_ICUDATA_BINARY_DIR}/brkitr res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/rbnf -d ${_ICUDATA_BINARY_DIR}/rbnf res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/unit -d ${_ICUDATA_BINARY_DIR}/unit res_index.txt
+        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${_ICUDATA_BINARY_DIR} -s generated/curr -d ${_ICUDATA_BINARY_DIR}/curr res_index.txt
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
+        DEPENDS ${_ICUDATA_BINARY_DIR}
+    )
 
-    set(locales_files root.txt da.txt de.txt en.txt eo.txt es.txt fi.txt fr.txt he.txt id.txt it.txt ko.txt my.txt nl.txt
+    set(locales_input_files root.txt da.txt de.txt en.txt eo.txt es.txt fi.txt fr.txt he.txt id.txt it.txt ko.txt my.txt nl.txt
         pl.txt pt.txt pt_PT.txt ro.txt ru.txt sv.txt tr.txt uk.txt vi.txt zh.txt zh_Hans.txt ars.txt az_AZ.txt bs_BA.txt
         en_NH.txt en_RH.txt in.txt in_ID.txt iw.txt iw_IL.txt ja_JP_TRADITIONAL.txt mo.txt no.txt no_NO.txt no_NO_NY.txt
         pa_IN.txt pa_PK.txt sh.txt sh_BA.txt sh_CS.txt sh_YU.txt shi_MA.txt sr_BA.txt sr_CS.txt sr_Cyrl_CS.txt
         sr_Cyrl_YU.txt sr_Latn_CS.txt sr_Latn_YU.txt sr_ME.txt sr_RS.txt sr_XK.txt sr_YU.txt th_TH_TRADITIONAL.txt tl.txt
         tl_PH.txt uz_AF.txt uz_UZ.txt vai_LR.txt yue_CN.txt yue_HK.txt zh_CN.txt zh_HK.txt zh_MO.txt zh_SG.txt zh_TW.txt)
 
-    set(curr_files root.txt supplementalData.txt da.txt de.txt en.txt eo.txt es.txt fi.txt fr.txt he.txt id.txt it.txt
+    set(curr_input_files root.txt supplementalData.txt da.txt de.txt en.txt eo.txt es.txt fi.txt fr.txt he.txt id.txt it.txt
         ko.txt my.txt nl.txt pl.txt pt.txt pt_PT.txt ro.txt ru.txt sv.txt tr.txt uk.txt vi.txt zh.txt zh_Hans.txt ar_SA.txt
         ars.txt az_AZ.txt az_Latn_AZ.txt bs_BA.txt bs_Latn_BA.txt en_NH.txt en_RH.txt fil_PH.txt he_IL.txt id_ID.txt in.txt
         in_ID.txt iw.txt iw_IL.txt ja_JP.txt ja_JP_TRADITIONAL.txt mo.txt nb_NO.txt nn_NO.txt no.txt no_NO.txt no_NO_NY.txt
@@ -53,7 +68,7 @@ if (ICU_PKGDATA_EXECUTABLE)
         yue_CN.txt yue_HK.txt yue_Hans_CN.txt yue_Hant_HK.txt zh_CN.txt zh_HK.txt zh_Hans_CN.txt zh_Hant_TW.txt zh_MO.txt
         zh_SG.txt zh_TW.txt)
 
-    set(unit_files root.txt da.txt de.txt en.txt eo.txt es.txt fi.txt fr.txt he.txt id.txt it.txt ko.txt my.txt nl.txt
+    set(unit_input_files root.txt da.txt de.txt en.txt eo.txt es.txt fi.txt fr.txt he.txt id.txt it.txt ko.txt my.txt nl.txt
         pl.txt pt.txt pt_PT.txt ro.txt ru.txt sv.txt tr.txt uk.txt vi.txt zh.txt zh_Hans.txt ar_SA.txt ars.txt az_AZ.txt
         az_Latn_AZ.txt bs_BA.txt bs_Latn_BA.txt en_NH.txt en_RH.txt fil_PH.txt he_IL.txt id_ID.txt in.txt in_ID.txt iw.txt
         iw_IL.txt ja_JP.txt ja_JP_TRADITIONAL.txt mo.txt nb_NO.txt nn_NO.txt no.txt no_NO.txt no_NO_NY.txt pa_Arab_PK.txt
@@ -63,58 +78,53 @@ if (ICU_PKGDATA_EXECUTABLE)
         tl.txt tl_PH.txt uz_AF.txt uz_Arab_AF.txt uz_Latn_UZ.txt uz_UZ.txt vai_LR.txt vai_Vaii_LR.txt yue_CN.txt yue_HK.txt
         yue_Hans_CN.txt yue_Hant_HK.txt zh_CN.txt zh_HK.txt zh_Hans_CN.txt zh_Hant_TW.txt zh_MO.txt zh_SG.txt zh_TW.txt)
 
-    add_icudata_resource_target(lang)
-    add_icudata_resource_target(region)
-    add_icudata_resource_target(zone)
-    add_icudata_resource_target(coll)
-    add_icudata_resource_target(brkitr)
-    add_icudata_resource_target(rbnf)
+    set(resource_bundle_output_files)
 
-    add_icudata_resource_target(unit)
-    add_icudata_resource_files(unit)
+    function(generate_icudata_resource_bundle target destination_path)
+        foreach(input_file IN LISTS ${target}_input_files)
+            string(REPLACE ".txt" ".res" output_file ${input_file})
+            list(APPEND resource_bundle_output_files "${destination_path}/${output_file}")
+            set (resource_bundle_output_files ${resource_bundle_output_files} PARENT_SCOPE)
+            add_custom_command(
+                OUTPUT ${destination_path}/${output_file}
+                COMMAND ${ICU_GENRB_EXECUTABLE} --usePoolBundle -k -i ${_ICUDATA_BINARY_DIR} -s ${target} -d ${destination_path} ${input_file}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
+                DEPENDS ${_ICUDATA_BINARY_DIR}
+            )
+        endforeach()
+    endfunction()
 
-    add_icudata_resource_target(curr)
-    add_icudata_resource_files(curr)
+    generate_icudata_resource_bundle(locales ${_ICUDATA_BINARY_DIR})
+    generate_icudata_resource_bundle(curr ${_ICUDATA_BINARY_DIR}/curr)
+    generate_icudata_resource_bundle(unit ${_ICUDATA_BINARY_DIR}/unit)
 
-    add_custom_target(icudata-locales ALL
-        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/icudt63l
-        COMMAND ${ICU_GENRB_EXECUTABLE} -k -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s generated -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l res_index.txt
-        COMMAND test -f locales/pool.res && ${ICU_ICUPKG_EXECUTABLE} -tl locales/pool.res ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/pool.res || true
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
-    )
-
-    foreach(file IN LISTS locales_files)
-        add_custom_command(TARGET icudata-locales PRE_BUILD
-            COMMAND ${ICU_GENRB_EXECUTABLE} --usePoolBundle -k -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -s locales -d ${CMAKE_CURRENT_BINARY_DIR}/icudt63l ${file}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
-        )
-    endforeach()
-
-    add_custom_target(icudata-confusables ALL
-        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/icudt63l
-        COMMAND ${ICU_GENCFU_EXECUTABLE} -c -i ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -r unidata/confusables.txt -w unidata/confusablesWholeScript.txt -o ${CMAKE_CURRENT_BINARY_DIR}/icudt63l/confusables.cfu
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
-    )
-
-    add_custom_target(icudata-lst ALL
-        COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/icudata.lst
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/icudata.lst
         COMMAND find -not -type d -printf '%P\\n' > ${CMAKE_CURRENT_BINARY_DIR}/icudata.lst
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/icudt63l
-        DEPENDS icudata-misc icudata-lang icudata-region icudata-zone icudata-coll icudata-brkitr icudata-rbnf icudata-unit icudata-locales icudata-curr icudata-confusables
+        WORKING_DIRECTORY ${_ICUDATA_BINARY_DIR}
+        DEPENDS ${_ICUDATA_BINARY_DIR}/pool.res
+        DEPENDS ${_ICUDATA_BINARY_DIR}/res_index.res
+        DEPENDS ${resource_bundle_output_files}
+    )
+
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/libicudata.a
+        COMMAND ${ICU_PKGDATA_EXECUTABLE} -q -c -s ${_ICUDATA_BINARY_DIR} -d ${CMAKE_CURRENT_BINARY_DIR} -e icudt63  -T ${CMAKE_CURRENT_BINARY_DIR} -p icudt63l -m static -r 63.1 -L icudata ${CMAKE_CURRENT_BINARY_DIR}/icudata.lst
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
+        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/icudata.lst
     )
 
     add_custom_target(icudata-pkgdata ALL
-        COMMAND ${ICU_PKGDATA_EXECUTABLE} -q -c -s ${CMAKE_CURRENT_BINARY_DIR}/icudt63l -d ${CMAKE_CURRENT_BINARY_DIR} -e icudt63  -T ${CMAKE_CURRENT_BINARY_DIR} -p icudt63l -m static -r 63.1 -L icudata ${CMAKE_CURRENT_BINARY_DIR}/icudata.lst
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/data
-        DEPENDS icudata-lst
+        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/libicudata.a
     )
 
     add_library(icudata STATIC IMPORTED GLOBAL)
     set_target_properties(icudata PROPERTIES
         IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/libicudata.a
     )
-    add_dependencies(icudata icudata-pkgdata)
-
+    add_dependencies(icudata
+        icudata-pkgdata
+    )
 else()
     add_library(icudata STATIC
         ${CMAKE_CURRENT_SOURCE_DIR}/icu4c/source/stubdata/stubdata.cpp
